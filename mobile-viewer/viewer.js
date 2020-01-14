9ca5d1ee346900475a29732cea7943a9995a7f3f
keithau.ett <keithau.ett@gmail.com>
2020-01-14 16:21:34 +0800

    Update

--- mobile-viewer/viewer.js
+++ mobile-viewer/viewer.js
@@ -449,7 +449,7 @@ PDFViewerApplication.animationStartedPromise.then(function() {
   //xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://keithauetw.github.io');
   xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
   xhr.withCredentials = false;
-  xhr.setRequestHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  xhr.setRequestHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept, Authorization');
   xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   xhr.responseType = "blob";
   xhr.onload = function (e) {
