0a485b407ec52fd45321a46508e214d36f4411ee
keithau.ett <keithau.ett@gmail.com>
2020-01-14 16:30:14 +0800

    Update

--- mobile-viewer/viewer.js
+++ mobile-viewer/viewer.js
@@ -446,8 +446,8 @@ PDFViewerApplication.animationStartedPromise.then(function() {
   else {
     return;
   }
-  //xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://keithauetw.github.io');
-  xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+  xhr.setRequestHeader('Access-Control-Allow-Origin', 'https://keithauetw.github.io');
+  //xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
   xhr.withCredentials = true;
   xhr.setRequestHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept, Authorization');
   xhr.setRequestHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
