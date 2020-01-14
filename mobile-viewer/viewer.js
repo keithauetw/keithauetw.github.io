7325c533efbecbeb7547c965a14f9850ed4f0690
keithau.ett <keithau.ett@gmail.com>
2020-01-14 12:11:46 +0800

    Update

--- mobile-viewer/viewer.js
+++ mobile-viewer/viewer.js
@@ -28,8 +28,8 @@ var CMAP_PACKED = true;
 pdfjsLib.GlobalWorkerOptions.workerSrc =
   "../../node_modules/pdfjs-dist/build/pdf.worker.js";
 
-//var DEFAULT_URL = "https://www.invesco.com.hk/dam/jcr:09e987cb-2bba-4103-95d0-8f48a127ebef/IGW_MRF_Prospectus_KFS_20191231_E.pdf";
-var DEFAULT_URL = "compressed.tracemonkey-pldi-09.pdf";
+var DEFAULT_URL = "https://www.invesco.com.hk/dam/jcr:09e987cb-2bba-4103-95d0-8f48a127ebef/IGW_MRF_Prospectus_KFS_20191231_E.pdf";
+//var DEFAULT_URL = "compressed.tracemonkey-pldi-09.pdf";
 var DEFAULT_FILENAME = "fund_doc.pdf";
 var DEFAULT_SCALE_DELTA = 1.1;
 var MIN_SCALE = 0.25;
